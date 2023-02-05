2bc66173717b138ffffe653fb1356e49a8f0dc4d
Ianskev <112659831+Ianskev@users.noreply.github.com>
2023-02-05 17:27:36 -0500

    Update

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'poke',
         'USER':'admin',
-        'PASSWORD':'h{fbW6GJY~!2=5Ff',
+        'PASSWORD':'h{fbW6GJY~!2=5Fff',
         'HOST':'127.0.0.1',
         'PORT':'5432',
     }
