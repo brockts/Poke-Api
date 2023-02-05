c7a56a4e997b5c86ea542037dbd5500e89e4adfb
Ianskev <112659831+Ianskev@users.noreply.github.com>
2023-02-05 17:23:42 -0500

    Update

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Test(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
--- myproject/settings.py
+++ myproject/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER':'poke',
-        'PASSWORD':'condori2004',
+        'NAME':'poke',
+        'USER':'admin',
+        'PASSWORD':'h{fbW6GJY~!2=5Ff',
         'HOST':'127.0.0.1',
         'PORT':'5432',
     }
