4c537e3b5b28ae1b62459b73441e76e106c58e0e
Ianskev <112659831+Ianskev@users.noreply.github.com>
2023-02-07 00:36:10 -0500

    pokemon_id & pokemon_name module improvement

--- myapp/views.py
+++ myapp/views.py
@@ -1,47 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import urllib.request,json,requests
+import json,requests
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world!")
+
+
 def pokemon_all(request):
-    url = "https://pokeapi.co/api/v2/pokemon/"
-    solicitud = requests.get(url)
-    data = solicitud.json()
+    url:str = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1008"
+    response = requests.get(url)
+    data = response.json()
     lista_pokemons = [pokemon["name"] for pokemon in data["results"]]
     return render(request,'pokemon_all.html',{
         "pokemons":lista_pokemons
     })
 
 
-def pokemon_by_id(request,id):
-    url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokemon/{id}/')
-    url_pokeapi.add_header('User-Agent', "pikachu")
-    source = urllib.request.urlopen(url_pokeapi).read()
-    data_list = json.loads(source)
-    data = {
-        "id": str(data_list['id']),
-        "name": str(data_list['name']).capitalize(),
-        "height": str(round(float(data_list["height"])*0.1,2))+ " m",
-        "weight": str(round(float(data_list["weight"])*0.1,2))+ " kg",
-        "sprite": str(data_list['sprites']['front_default']),
-        }
-    return render(request, 'pokemon_id.html',data)
+def pokemon_by_id(request,id:int):
+    url_pokeapi:str = f"https://pokeapi.co/api/v2/pokemon/{id}"
+    response = requests.get(url_pokeapi)
+    data = response.json()
+    database:dict = {
+        "id":data["id"],
+        "name":data["name"],
+        "weight":data["weight"],
+        "height": data["height"],
+        "abilities": [skill["ability"]["name"] for skill in data["abilities"]]
+    }
+    sprite:str = data['sprites']['front_default']
+    return render(request, 'pokemon_id.html',{
+        "database":database,
+        "sprite":sprite
+    })
 
-def pokemon_by_name(request,name):
-    url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokemon/{name.casefold()}/')
-    url_pokeapi.add_header('User-Agent', "pikachu")
-    source = urllib.request.urlopen(url_pokeapi).read()
-    data_list = json.loads(source)
-    data = {
-        "id": str(data_list['id']),
-        "name": str(data_list['name']).capitalize(),
-        "height": str(round(float(data_list["height"])*0.1,2))+ " m",
-        "weight": str(round(float(data_list["weight"])*0.1,2))+ " kg",
-        "sprite": str(data_list['sprites']['front_default']),
-        }
-    return render(request, 'pokemon_name.html',data)
+
+def pokemon_by_name(request,name:str):
+    url_pokeapi:str = f"https://pokeapi.co/api/v2/pokemon/{name.casefold()}"
+    response = requests.get(url_pokeapi)
+    data = response.json()
+    database:dict = {
+        "id":data["id"],
+        "name":data["name"],
+        "weight":data["weight"],
+        "height": data["height"],
+        "abilities": [skill["ability"]["name"] for skill in data["abilities"]]
+    }
+    sprite:str = data['sprites']['front_default']
+    return render(request, 'pokemon_name.html',{
+        "database":database,
+        "sprite":sprite
+    })
 def pokedex_by_id(request,id):
     url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokedex/{id}/')
     url_pokeapi.add_header('User-Agent', "pikachu")
