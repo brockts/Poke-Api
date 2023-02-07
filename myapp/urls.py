501328eb705e42681500a7dee36e4e73478967cc
Ianskev <112659831+Ianskev@users.noreply.github.com>
2023-02-06 23:09:32 -0500

    List of all pokemons

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pokemon/',views.pokemon_all,name="pokemon"),
     path('pokemon/<int:id>/', views.pokemon_by_id),
     path('pokemon/<str:name>/', views.pokemon_by_name),
-    path('pokedex/<int:id>/',views.pokedex_by_id)
+    path('pokedex/<int:id>/',views.pokedex_by_id),
+    path('pokedex/<str:name>/',views.pokedex_by_name)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import urllib.request,json
+import urllib.request,json,requests
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world!")
+def pokemon_all(request):
+    url = "https://pokeapi.co/api/v2/pokemon/"
+    solicitud = requests.get(url)
+    data = solicitud.json()
+    lista_pokemons = [pokemon["name"] for pokemon in data["results"]]
+    return render(request,'pokemon_all.html',{
+        "pokemons":lista_pokemons
+    })
+
+
 def pokemon_by_id(request,id):
     url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokemon/{id}/')
     url_pokeapi.add_header('User-Agent', "pikachu")
@@ -33,6 +43,18 @@ def pokemon_by_name(request,name):
         }
     return render(request, 'pokemon_name.html',data)
 def pokedex_by_id(request,id):
+    url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokedex/{id}/')
+    url_pokeapi.add_header('User-Agent', "pikachu")
+    source = urllib.request.urlopen(url_pokeapi).read()
+    data_list = json.loads(source)
+    database = {
+        "description": str(data_list['descriptions'][2]["description"]),
+        "pokemon_entries": str(data_list['pokemon_entries'])
+    }
+    return render(request,"pokedex_id.html",{
+        "database":database
+    })
+def pokedex_by_name(request,name):
     url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokedex/{id}/')
     url_pokeapi.add_header('User-Agent', "pikachu")
     source = urllib.request.urlopen(url_pokeapi).read()
